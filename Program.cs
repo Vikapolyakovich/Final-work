06f36a16df321f13bb1170bc7a43d77d4097978a
Viktoria Polyakovich <polyakovichvika@yandex.by>
2023-07-10 21:46:09 +0300

    added program final work

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+void FillArray(string[] array)
+{
+    string[] new_array = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            new_array[i] = array[i];
+        }
+    }
+    PrintArray(array);
+    PrintArray(new_array);
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(array[i] + " "); 
+    }
+    Console.WriteLine();
+}
+
+string[] array = {"Hello", "2", "world", ":-)"};
+
+FillArray(array);
